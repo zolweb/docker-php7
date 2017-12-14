c0e394d84529f68de8f2c6dc76a7b6716415657f
Christophe Hautenne <christophe.hautenne@zol.fr>
2017-12-14 15:00:37 +0100

    Update readme and app rights inside symfony project

--- 3.0/script/start.sh
+++ 3.0/script/start.sh
@@ -2,34 +2,48 @@
 set -o errexit
 
 php_logs() {
+    local conf_file="/usr/local/etc/php-fpm.d/www.conf"
+
+    local log_dir="/var/log/php"
+    local access_log_file="${log_dir}/access.log"
+    local error_log_file="${log_dir}/error.log"
+
+    mkdir -p ${log_dir} \
+        && touch ${access_log_file} \
+        && touch ${error_log_file};
+
     if [ ! -z "$UID" ] && [ ! -z "$GID" ]; then
-        local conf_file="/usr/local/etc/php-fpm.d/www.conf"
-
-        local log_dir="/var/log/php"
-        local access_log_file="${log_dir}/access.log"
-        local error_log_file="${log_dir}/error.log"
-
-        mkdir -p ${log_dir} \
-            && touch ${access_log_file} \
-            && touch ${error_log_file} \
-            && chown -R $UID:$GID ${log_dir}
-
-        # The "c" supplementary letter is needed because the first letter will be cut at replacement.
-        sed -i '/^;catch_workers_output/ccatch_workers_output = yes' ${conf_file} \
-            && sed -i '/^;log_level/clog_level = debug' ${conf_file} \
-            && sed -i '/^;listen/clisten = 9000' ${conf_file} \
-            && sed -i '/^;access.log/caccess.log = /var/log/php/access.log' ${conf_file} \
-            && sed -i '/^;php_flag\[display_errors\]/cphp_flag[display_errors] = off' ${conf_file} \
-            && sed -i '/^;php_admin_value\[error_log\]/cphp_admin_value[error_log] = /var/log/php/error.log' ${conf_file} \
-            && sed -i '/^;php_admin_flag\[log_errors\]/cphp_admin_flag[log_errors] = on' ${conf_file} \
-            && sed -i '/^;clear_env/cclear_env = no' ${conf_file}
+        chown -R $UID:$GID ${log_dir}
     fi
 
-    setfacl -R  -m u:"www-data":rwX -m u:"root":rwX /var/www/html
-    setfacl -dR -m u:"www-data":rwX -m u:"root":rwX /var/www/html
+    # The "c" supplementary letter is needed because the first letter will be cut at replacement.
+    sed -i '/^;catch_workers_output/ccatch_workers_output = yes' ${conf_file} \
+        && sed -i '/^;log_level/clog_level = debug' ${conf_file} \
+        && sed -i '/^;listen/clisten = 9000' ${conf_file} \
+        && sed -i '/^;access.log/caccess.log = /var/log/php/access.log' ${conf_file} \
+        && sed -i '/^;php_flag\[display_errors\]/cphp_flag[display_errors] = off' ${conf_file} \
+        && sed -i '/^;php_admin_value\[error_log\]/cphp_admin_value[error_log] = /var/log/php/error.log' ${conf_file} \
+        && sed -i '/^;php_admin_flag\[log_errors\]/cphp_admin_flag[log_errors] = on' ${conf_file} \
+        && sed -i '/^;clear_env/cclear_env = no' ${conf_file}
+}
+
+symfony_logs() {
+    local html_dir="/var/www/html"
+    local var_dir="${html_dir}/var"
+
+    if [ ! -z "$UID" ] && [ ! -z "$GID" ]; then
+        # To avoid permissions issues, create directly the /var/www/html/var directory and give it to $UID:$GID
+
+        mkdir -p ${var_dir}
+        chown -R $UID:$GID ${var_dir}
+    fi
+
+    setfacl -R  -m u:"www-data":rwX -m u:"root":rwX ${html_dir}
+    setfacl -dR -m u:"www-data":rwX -m u:"root":rwX ${html_dir}
 }
 
 php_logs
+symfony_logs
 
 cron -L 15
 php-fpm --nodaemonize
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # docker-php7
 A docker image with php 7 and extensions (apc, apcu, intl, mcrypt,...)
 
-```
-docker build -t zolweb/php7:1.0 1.0
-```
+See docker hub image [zolweb/php7](https://hub.docker.com/r/zolweb/php7/)
+
+
+| Version        | Docker                       | Symfony recommended version |
+|:--------------:|:----------------------------:|:---------------------------:|
+| 1.0            | php:7.0-fpm                  | 2.x -> 3.x                  |
+| 1.1            | php:7.0-fpm                  | 2.x -> 3.x                  |
+| 2.0-alpine     | php:7.0.11-fpm-alpine        | 2.x -> 3.x                  |
+| 2.1-alpine     | php:7.0.11-fpm-alpine        | 2.x -> 3.x                  |
+| 2.2-alpine     | php:7.1.1-fpm-alpine         | 2.x -> 3.x                  |
+| 3.0            | php:7.1-fpm                  | 4.x                         |
+
+1.1 comes with libfreetype6-dev, libjpeg62-turbo-dev and libpng12-dev comparing to 1.0.
+From 2.x, logrotate have been added
+
